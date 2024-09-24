0be005b1a631b9978a76d4c99faba8bd26f58715
Prashanth.Hegde <Prashanth.Hegde@target.com>
2024-09-23 20:08:11 -0500

    refactor: cleanup and add upload endpoint

--- src/main.v
+++ src/main.v
@@ -61,64 +61,21 @@ pub fn (app &App) root() veb.Result {
 	return $veb.html()
 }
 
-/// render_file renders a file to the response
-/// If the file is a video for example, it will be rendered as mimetype mp4 so that the browser can play it directly.
-/// Same with other extensions
-// fn (app &App) render_file(filename string) veb.Result {
-// 	return if os.file_ext(filename) !in veb.mime_types && os.file_size(filename) < 500_000 {
-// 		ctx.text(os.read_file(filename) or { 'unable to read file contents' })
-// 	} else if os.file_ext(filename) in veb.mime_types {
-// 		ctx.file(filename)
-// 	} else {
-// 		ctx.text('either file too large or unknown mime type')
-// 	}
-// }
+@['/upload'; post]
+pub fn (mut app App) upload() veb.Result {
+	file_data := ctx.files['file'][0]
+	path := ctx.form['path']
 
-/*
-// endpoints
-@['/']
-pub fn (mut app App) root() veb.Result {
-	all_shares := *opts
-	shares := all_shares.map(it.name)
-	path := app.query['path'] or { '' }
-	mut files := []File{}
-	mut upload := false
-
-	serve_opt := get_options(all_shares, path) or { return $veb.html() }
-	upload = serve_opt.upload
-	resolved_path := serve_opt.resolve_path(path)
-	if os.is_file(resolved_path) {
-		return if os.file_ext(resolved_path) !in veb.mime_types
-			&& os.file_size(resolved_path) < 500_000 {
-			app.text(os.read_file(resolved_path) or { 'unable to read file contents' })
-		} else if os.file_ext(resolved_path) in veb.mime_types {
-			app.file(resolved_path)
-		} else {
-			app.text('either file too large or unknown mime type')
+	if file_data.data.len > 0 && file_data.filename != '' {
+		all_shares := app.opts
+		opt := all_shares.opt(path)
+		resolved_path := os.join_path(opt.resolve_path(path), file_data.filename)
+		println('writing new file to ${resolved_path}')
+		os.write_file(resolved_path, file_data.data) or {
+			eprintln('error writing file ${file_data.filename}')
+			return ctx.redirect('/${path}')
 		}
 	}
 
-	files = stat(resolved_path)
-	files = filter(files, serve_opt)
-	return $veb.html()
+	return ctx.redirect('/${path}')
 }
-*/
-
-// @['/upload'; post]
-// pub fn (mut app App) upload() veb.Result {
-// 	file_data := app.Context.files['file'][0]
-// 	path := app.Context.form['path']
-
-// 	if file_data.data.len > 0 && file_data.filename != '' {
-// 		all_shares := *opts
-// 		serve_opt := get_options(all_shares, path) or { return app.redirect('/${path}') }
-// 		resolved_path := os.join_path(serve_opt.resolve_path(path), file_data.filename)
-// 		println('writing new file to ${resolved_path}')
-// 		os.write_file(resolved_path, file_data.data) or {
-// 			eprintln('error writing file ${file_data.filename}')
-// 			return app.redirect('/${path}')
-// 		}
-// 	}
-
-// 	return app.redirect('/${path}')
-// }
